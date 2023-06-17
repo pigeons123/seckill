9c22c7660fe67c10fe6907eab8e2230fb082d1ca
binghe001 <1028386804@qq.com>
2023-06-17 11:49:31 +0800

    feat: 秒杀商品监听缓存领域事件的设计与实现

---
+++ seckill-application/src/main/java/io/binghe/seckill/application/event/handler/SeckillGoodsEventHandler.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.seckill.application.event.handler;
+
+import com.alibaba.cola.dto.Response;
+import com.alibaba.cola.event.EventHandler;
+import com.alibaba.cola.event.EventHandlerI;
+import com.alibaba.fastjson.JSON;
+import io.binghe.seckill.application.cache.service.goods.SeckillGoodsCacheService;
+import io.binghe.seckill.application.cache.service.goods.SeckillGoodsListCacheService;
+import io.binghe.seckill.domain.event.SeckillGoodsEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author binghe(微信 : hacker_binghe)
+ * @version 1.0.0
+ * @description 商品事件处理器
+ * @github https://github.com/binghe001
+ * @copyright 公众号: 冰河技术
+ */
+@EventHandler
+public class SeckillGoodsEventHandler implements EventHandlerI<Response, SeckillGoodsEvent> {
+    private final Logger logger = LoggerFactory.getLogger(SeckillGoodsEventHandler.class);
+
+    @Autowired
+    private SeckillGoodsCacheService seckillGoodsCacheService;
+    @Autowired
+    private SeckillGoodsListCacheService seckillGoodsListCacheService;
+
+    @Override
+    public Response execute(SeckillGoodsEvent seckillGoodsEvent) {
+        logger.info("goodsEvent|接收秒杀品事件|{}", JSON.toJSON(seckillGoodsEvent));
+        if (seckillGoodsEvent.getId() == null){
+            logger.info("goodsEvent|接收秒杀品事件参数错误");
+            return Response.buildSuccess();
+        }
+        seckillGoodsCacheService.tryUpdateSeckillGoodsCacheByLock(seckillGoodsEvent.getId());
+        seckillGoodsListCacheService.tryUpdateSeckillGoodsCacheByLock(seckillGoodsEvent.getActivityId());
+        return Response.buildSuccess();
+    }
+}
